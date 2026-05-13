ec200f60bd695fa72ed6452a013fb138690c45f6
Yusadinar <yusadinar8@gmail.com>
2026-05-13 15:15:59 +0700

    Menghapus file PHP Debug project

--- test_update.php
+++
@@ -1,9 +0,0 @@
-<?php
-$ia = App\Models\InternalAgreement::find(5);
-if ($ia) {
-    $ia->approval_step = 3;
-    $ia->save();
-    echo "Updated IA 5 to step 3\n";
-} else {
-    echo "IA 5 not found\n";
-}
